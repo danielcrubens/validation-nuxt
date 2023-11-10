56f92e061644906b90412dae2ee02d9e887c815d
danielcrubens <dcr.tecnologi@gmail.com>
2023-11-10 10:40:24 -0300

    adicionando apges e components :fire:

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <VTextInput />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
