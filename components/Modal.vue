0d167aaee5289e08ae89df7beb0a7169393dbb70
danielcrubens <dcr.tecnologi@gmail.com>
2023-11-19 22:24:22 -0300

    refatorando lógica envio dos dados

---
+++ components/Modal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="modal" v-show="showModal">
+    <div class="modal-content">
+      <!-- Aqui você pode exibir o conteúdo da página -->
+      <!-- Por exemplo, usando um iframe -->
+      <iframe :src="modalContentUrl" frameborder="0" width="100%" height="100%"></iframe>
+      <button @click="closeModal">Fechar Modal</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showModal: false,
+      modalContentUrl: '' // Aqui será definida a URL da página que será exibida no modal
+    };
+  },
+  methods: {
+    openModal(url) {
+      this.modalContentUrl = url;
+      this.showModal = true;
+    },
+    closeModal() {
+      this.modalContentUrl = '';
+      this.showModal = false;
+    }
+  }
+};
+</script>
+
+<style>
+/* Estilos do modal, ajuste conforme necessário */
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 9999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  max-width: 80%;
+  max-height: 80%;
+  overflow: auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,115 @@
 <template>
-  <div class="w-11/12 lg:w-10/12  mx-auto px-2">
+  <div class="w-11/12 lg:w-6/12  mx-auto px-2">
+    <Form @submit="onFormSubmit">
 
-    <Form method="post" action="https://formsubmit.co/daniel.rubens@cadmus.com.br" :validation-schema="schema">
-      <input type="hidden" name="_next" value="https://www.uol.com.br/">
-      <div class="">
-        <Field name="name" type="text"
+      <div class="mb-5">
+        <Field
           class="appearance-none block w-full bg-gray-200 text-gray border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-          placeholder="Seu nome" rules="required" />
+          name="name" v-model="form.name" type="text" placeholder="Seu nome" rules="required" />
         <ErrorMessage name="name" class="text-red-500 text-sm" />
       </div>
 
-      <div>
-        <Field name="email" type="email"
+      <div class="mb-5">
+        <Field
           class="appearance-none block w-full bg-gray-200 text-gray border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-          placeholder="Seu e-mail" rules="required|email" />
+          name="email" v-model="form.email" type="email" placeholder="Seu e-mail" rules="required|email" />
         <ErrorMessage name="email" class="text-red-500 text-sm" />
+      </div>
+
+      <div class="mb-5">
+        <Field
+          class="appearance-none block w-full bg-gray-200 text-gray border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+          name="subject" v-model="form.subject" type="text" placeholder="Assunto" rules="required" />
+        <ErrorMessage name="subject" class="text-red-500 text-sm" />
+      </div>
+
+      <div class="mb-5">
+        <Field
+          class="appearance-none block w-full bg-gray-200 text-gray border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+          v-maska data-maska="(##) #####-####" type="tel" v-model="form.phone" name="phone" placeholder="(XX) XXXX-XXXX"
+          rules="required" />
+        <ErrorMessage name="phone" class="text-red-500 text-sm" />
+      </div>
+
+      <div class="mb-5">
+        <Field
+          class="appearance-none block w-full bg-gray-200 text-gray border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+          as="textarea" name="message" placeholder="Sua mensagem" v-model="form.message" rows="6" rules="required" />
+        <ErrorMessage name="message" class="text-red-500 text-sm" />
       </div>
 
       <button
         class="inline-block bg-blue-500 w-full rounded-lg mt-6 transition duration-300 ease-in-out px-10 py-3 text-lg text-white font-bold sm:w-auto"
-        type="submit">Foi</button>
+        type="submit">
+        <template v-if="waiting">Carregando</template>
+        <template v-if="!waiting">Enviar</template>
+      </button>
     </Form>
+    <p v-if="showError" class="text-red-500">Falha ao enviar o formulário.</p>
+    <p v-if="success" class="text-green-500">Formulário enviado com sucesso!</p>
+
   </div>
 </template>
- 
+  
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const form = ref({
+  name: '',
+  email: '',
+  subject: '',
+  phone: '',
+  message: '',
+});
+
+const errors = ref(false);
+const success = ref(false);
+const waiting = ref(false);
+
+const showError = computed(() => errors.value && !success.value);
+
+function onFormSubmit() {
+  // Aqui o Vee Validate automaticamente valida o formulário
+  if (form.value.name && form.value.email && form.value.subject && form.value.phone && form.value.message) {
+    sendFormData();
+  } else {
+    errors.value = true;
+    success.value = false;
+  }
+}
+
+async function sendFormData() {
+  waiting.value = true;
+  try {
+    const response = await fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form.value),
+    });
+
+    if (response.ok) {
+      errors.value = false;
+      success.value = true;
+      form.value = {
+        name: '',
+        email: '',
+        subject: '',
+        phone: '',
+        message: '',
+      };
+    } else {
+      throw new Error('Failed to submit');
+    }
+  } catch (error) {
+    errors.value = true;
+    success.value = false;
+  } finally {
+    waiting.value = false;
+  }
+}
+</script>
+
 
